d26a19f2dbc5ccca91cf05b3dbd054f8dc2d9136
randy steven <randysteven12@gmail.com>
2024-02-13 23:10:21 +0700

    assignment 2

--- assignment_2.py
+++ assignment_2.py
@@ -3,13 +3,30 @@ from selenium.webdriver.common.by import By
 
 
 class SeleniumAssignment:
-    driver = webdriver.Chrome()
+    def __init__(self):
+        self.driver = webdriver.Chrome()
 
     def open_driver(self):
         self.driver.get("https://www.saucedemo.com/")
 
     def login_user(self):
         user_name = self.driver.find_element(By.ID, "user-name")
-        password = self.driver.find_element(By.ID, "password")
-        user_name.send_keys("standard_user")
-        password.send_keys('secret_sauce')
+        password_field = self.driver.find_element(By.ID, "password")
+        login_button = self.driver.find_element(By.ID, "login-button")
+
+        user_name.send_keys(username)
+        password_field.send_keys(password)
+        login_button.click()
+
+    def add_items_to_cart(self):
+        inventory_items = self.driver.find_elements(By.CLASS_NAME, "inventory_item")
+        for item in inventory_items:
+            add_to_cart_button = item.find_element(By.CLASS_NAME, "btn_inventory")
+            add_to_cart_button.click()
+
+    def proceed_to_checkout(self):
+        cart_icon = self.driver.find_element(By.CLASS_NAME, "shopping_cart_container")
+        cart_icon.click()
+        checkout_button = self.driver.find_element(By.CSS_SELECTOR, ".checkout_button")
+        checkout_button.click()
+
